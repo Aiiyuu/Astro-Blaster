d2787c7811c6cae7700985b00b0f669cd67829a2
Nazariy <nazariy.holovach2007@gmail.com>
2025-06-08 02:17:40 +0300

    Updated the typescript configuration

---
+++ src/scripts/config.ts
@@ -0,0 +1,12 @@
+/**
+ * config.ts
+ *
+ * Centralized configuration and constants for the game such as canvas size,
+ * speeds, colors, and other gameplay settings.
+ */
+
+
+export const config = {
+    canvasWidth: 800,
+    canvasHeight: 600,
+}
